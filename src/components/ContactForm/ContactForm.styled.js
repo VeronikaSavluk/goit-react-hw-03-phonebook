9b17e1945d480e92d4bfb58e231275668c2eb727
Veronika Savluk <s_veronica@ukr.net>
2022-09-26 02:49:01 +0300

    Create ContactForm.styled.js

---
+++ src/components/ContactForm/ContactForm.styled.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.form`
+display: flex;
+flex-direction: column;
+margin: 0;
+padding: 10px;
+border: 2px solid #004D00;
+border-radius: 15px;
+`;
+
+export const NameLable = styled.label`
+display: flex;
+flex-direction: column;
+margin-bottom: 10px;
+font-size: 16px;
+`;
+
+export const NameInput = styled.input`
+margin-bottom: 10px;
+padding: 4px;
+font-size: 12px;
+background: #FFF8DC;
+border: none;
+border-radius: 3px;
+;`
+
+export const SubitForm = styled.button`
+margin-top: 10px;
+margin-left: auto;
+padding: 4px;
+width: 110px;
+font-weight: 500;
+border: 1px solid #1E90FF;
+border-radius: 15px;
+background-color: #1E90FF;
+color: #FFF8DC;
+`;
